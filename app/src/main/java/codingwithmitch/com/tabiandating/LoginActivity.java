75a05c617ad34c15795c137be3adabff5f8f4f02
Jason Nguyen <jasonvex@gmail.com>
2018-03-16 19:20:34 -0500

    first commit

---
+++ app/src/main/java/codingwithmitch/com/tabiandating/LoginActivity.java
@@ -0,0 +1,35 @@
+package codingwithmitch.com.tabiandating;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = "LoginActivity";
+
+    //widgets
+    private Button mLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        mLogin = findViewById(R.id.btn_login);
+
+        mLogin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btn_login) {
+            Log.d(TAG, "onClick: logging in the user");
+
+//            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+
+        }
+    }
+}
